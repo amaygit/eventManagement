008c02dfa1f6d014a8d2860049e7530a5f93e168
unknown <mailamaynayak@gmail.com>
2023-12-19 13:17:36 +0530

    commite

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,8 +1,7 @@
 import { IEvent } from '@/lib/database/models/event.model'
 import React from 'react'
-// import Card from './Card'
-import Pagination from './Pagination'
 import Card from './Card'
+import Pagination from './Pagination'
 
 type CollectionProps = {
   data: IEvent[],
